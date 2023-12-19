c0977d742ddcd66f78ade950c1acadfcf63ff391
yerman <yerman.ibragimuly@gmail.com>
2023-12-19 19:56:46 +0600

    Initial Commit

---
+++ sprint3_yermanibragimuly/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  sprint3_yermanibragimuly
+//
+//  Created by Yerman Ibragimuly on 19.12.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
